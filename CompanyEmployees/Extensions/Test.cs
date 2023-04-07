b69dbb73ea8b7ce38ca56742f85be3ac8188d0a2
Nnamani Stephen <sir.stephennnamani@gmail.com>
2023-04-07 07:57:13 +0100

    Initial commit

---
+++ CompanyEmployees/Extensions/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace CompanyEmployees.Extensions
+{
+    public static class Test
+    {
+        public static void Text(string name, int age)
+        {
+            Console.WriteLine($"My name is {name}");
+            var currentYear = DateTime.Now.Year;
+            var DOB = currentYear - age;
+            Console.WriteLine($"Age is:\t{DOB}");
+        }
+    }
+}
---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,69 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions { ForwardedHeaders = ForwardedHeaders.All });
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.Use(async (context, next) => { 
+    await context.Response.WriteAsync("(First) Hello from the middleware component. \t"); 
+    await next.Invoke(); 
+    Console.WriteLine($"Logic after executing the next delegate in the Use method");
+    await next.Invoke();
+    Test.Text("Stephen Nnamani", 32);
+}); 
+//app.Run(async context => { 
+//    Console.WriteLine($"Writing the response to the client in the Run method"); 
+//    //context.Response.StatusCode = 200; 
+//    await context.Response.WriteAsync("Hello from the middleware component."); });
+app.Map("/roboticSence", builder =>
+{
+    builder.Use(async (context, next) =>
+    {
+        Console.WriteLine("Map.Use");
+        await next.Invoke();
+        Console.WriteLine("Map.Use after next delegate");
+    });
+    builder.Run(async context =>
+    {
+        Console.WriteLine($"Map branch response to the client in the Run method");
+        await context.Response.WriteAsync("This is from the Robotic sence\t");
+        await context.Response.WriteAsync("Hello from the builder.Run map branch.\t");
+    });
+});
+
+app.Run(async context => { Console.WriteLine($"Writing the response to the client in the Run method"); 
+    await context.Response.WriteAsync("(last)Hello from the App.Run component. "); 
+});
+
+app.MapControllers();
+
+app.Run();
